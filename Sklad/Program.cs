71c595d7c2412cb966dda9f51353e49c2bbefbd2
lirikofhell <63696809+lirikofhell@users.noreply.github.com>
2021-04-23 06:15:07 +0300

    Add files via upload

---
+++ Sklad/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Sklad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxProduct = 100;
+            List<Product> products = new List<Product>();
+            //products.Add(new Product{ productName = "milk", productVolume = 1 });
+            //products.Add(new Product { productName = "sugar", productVolume = 2 });
+            //products.Add(new Product { productName = "salt", productVolume = 3 });
+            //products.Add(new Product { productName = "pig", productVolume = 4 });
+            //products.Add(new Product { productName = "beef", productVolume = 5 });
+            Store store = new Store(true, 0, maxProduct);
+            Thread thPutProduct = new Thread(new ThreadStart(store.PutProduct));
+            Thread thGetProduct = new Thread(new ThreadStart(store.GetProduct));
+            Thread thGetProductCount = new Thread(new ThreadStart(store.GetProductCount));
+            thGetProduct.Start();
+            thPutProduct.Start();
+            thGetProductCount.Start();
+        }
+    }
+
+    public class Product : IEquatable<Product>
+    {
+        public string productName { get; set; }
+
+        public int productVolume { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + productName +  "    Volume: " + productVolume;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Product objAsPart = obj as Product;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public int getProductVolume()
+        {
+            return productVolume;
+        }
+        public bool Equals(Product other)
+        {
+            if (other == null) return false;
+            return (this.productName.Equals(other.productName));
+        }
+    }
+}
---
+++ Sklad/Store.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Sklad
+{
+    public class Store
+    {
+        readonly Boolean enabled = true;
+        private readonly object Lock = new object();
+        readonly Random rng = new Random();
+        List<string> products = new List<string>();
+        private int productVolume;
+        private int product = 0;
+        private int maxProduct;
+
+        public Store(bool enabled, int product, int maxProduct)
+        {
+            this.enabled = enabled;
+            this.product = product;
+            this.maxProduct = maxProduct;
+        }
+
+        public void PutProduct()
+        {
+            while (enabled)
+            {
+                if(product <= maxProduct)
+                {
+                    for (int i = 0; i < rng.Next(1, 5); i++)
+                    {
+                        lock (Lock)
+                        {
+                            productVolume = rng.Next(1, 6);
+                            product += productVolume;
+                            string productName = "p" + productVolume;
+                            products.Add(productName);
+                            Console.WriteLine("put" + " Name: " + productName + " Volume: " + productVolume);
+                        }
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        public void GetProduct()
+        {
+            while (enabled)
+            {
+                lock (Lock)
+                {
+                    for(int i=0; i<rng.Next(1, 5); i++)
+                    {
+                        productVolume = rng.Next(1, 6);
+                        string productName = "p" + productVolume;
+                        if (products.Contains(productName) == true)
+                        {
+                            product -= productVolume;
+                            products.Remove(productName);
+                            Console.WriteLine("get" + " Name: " + productName + " Volume: " + productVolume);
+                        }
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        public void GetProductCount()
+        {
+            while (enabled)
+            {
+                lock (Lock)
+                {
+                    Console.WriteLine(product);
+                }
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
